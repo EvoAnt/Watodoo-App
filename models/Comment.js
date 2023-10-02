5dd42e6091febfc99f8ce0496c595dbce20dd7fb
Anthony Lara <Capone0824@gmail.com>
2023-10-02 16:31:34 -0400

    started events code

---
+++ models/Comment.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    comment: { type: String, maxlength: 200 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Review", reviewSchema);
--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const Event = require("../models/Event");
 
-router.get('/all-events', (req, res, next) => {
-  res.render('events/all-events.hbs');
+router.get("/all-events", (req, res, next) => {
+  Event.find()
+    .populate("owner")
+    .then((events) => {
+      console.log("Found events ===>", events);
+      res.render("events/all-events.hbs", { events });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 });
+
+router.get('/new', (req, res, next) => {
+    
+})
+
 
 module.exports = router;
